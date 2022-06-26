c5b3f415c7d2b2a0b2cdd1f022f49f460a9d08ca
limhuiseong <limhuiseong.cs@gmail.com>
2022-06-26 14:30:27 +0900

    commit

---
+++ Introduction_to_Algoritms/22p.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int len;
+    cin >> len;
+
+    int arr[len+1];
+
+    int key;
+
+    for(int i = 1; i <= len; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int j = 2; j <= len; j++)
+    {
+        key = arr[j];
+
+        int i = j - 1;
+
+        while(i > 0 && arr[i] < key)
+        {
+            arr[i+1] = arr[i];
+            i = i - 1;
+        }
+        arr[i+1] =key;
+    }
+
+    for(int i = 1; i <= len; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
