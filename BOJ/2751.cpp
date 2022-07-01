eedb32aa2267e77d14205daa2e543973e78aa6c5
limhuiseong <limhuiseong.cs@gmail.com>
2022-07-01 16:23:22 +0900

    commit

--- BOJ/2751.cpp
+++ BOJ/2751.cpp
@@ -10,8 +10,49 @@ using namespace std;
 
 int sorted[1000000];
 
-void merge(int list[], int left, int mid, int right);
-void merge_sort(int list[], int left, int right);
+void merge(int list[], int left, int mid, int right)
+{
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    int l;
+
+    while(i <= mid && j <= right)
+    {
+        if(list[i] <= list[j])
+            sorted[k++] = list[i++];
+        else
+            sorted[k++] = list[j++];
+    }
+
+    if(i > mid)
+    {
+        for(l = j; l <= right; l++)
+            sorted[k++] = list[l];
+    }
+    else
+    {
+        for(l = i; l <= mid; l++)
+            sorted[k++] = list[l];
+    }
+    
+    for(l = left; l <= right; l++)
+        list[l] = sorted[l];
+}
+
+void merge_sort(int list[], int left, int right)
+{
+    int mid;
+
+    if(left < right)
+    {
+        mid = (left + right) / 2;
+        merge_sort(list, left, mid);
+        merge_sort(list, mid + 1, right);
+        merge(list, left, mid, right);
+    }
+}
+
 
 int main()
 {
@@ -31,43 +72,4 @@ int main()
         cout << list[i] << "\n";
 
     return 0;
-}
-
-void merge_sort(list[], left, right)
-{
-    int mid;
-
-    if(left < right)
-    {
-        mid = (left + right) / 2;
-        merge_sort(list, left, mid);
-        merge_sort(list, mid + 1, right);
-        merge(list, left, mid, right);
-    }
-}
-
-void merge(int list[], int left, int mid, int right)
-{
-    int i = left;
-    int j = mid + 1;
-    int k = left;
-    int l;
-
-    while(i <= mid && j <= right)
-    {
-        if(list[i] <= list[j])
-            sorted[k++] = list[i++];
-        else
-            sorted[k++] = ldsist[j++];
-    }
-
-    if(i > mid)
-    {
-        for(l = j; l <= right; l++)
-            sorted[k++] = list[l];
-    }
-    else
-    {
-        for(l = i)
-    }
 }
